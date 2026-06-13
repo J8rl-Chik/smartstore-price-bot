447839e649573744db60f4311d3d8a26a2d9d713
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-06-13 16:29:57 +0900

    feat: generateAccessToken 함수 추가
    - OAuth2 토큰 생성을 위한 generateAccessToken 함수 구현
    - 환경 변수에서 CLIENT_ID와 CLIENT_SECRET을 가져와서 사용
    - bcrypt를 이용한 비밀번호 해싱 및 base64 인코딩
    - Naver API에 POST 요청을 통해 access_token 반환

---
+++ smartStore/generateAccessToken.js
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import { pathToFileURL } from "node:url";
+import bcrypt from "bcrypt";
+import fetch from "node-fetch";
+
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  generateAccessToken().then(() => {
+    console.log("generateAccessToken 함수 테스트");
+  });
+}
+
+async function generateAccessToken() {
+  const { CLIENT_ID, CLIENT_SECRET } = process.env;
+  const timestamp = Date.now();
+  const password = `${CLIENT_ID}_${timestamp}`; // 밑줄로 연결하여 password 생성
+  const hashed = bcrypt.hashSync(password, CLIENT_SECRET); // bcrypt 해싱
+  const clientSecretSign = Buffer.from(hashed, "utf-8").toString("base64"); // base64 인코딩
+
+  const query = new URLSearchParams({
+    client_id: CLIENT_ID,
+    timestamp,
+    grant_type: "client_credentials",
+    client_secret_sign: clientSecretSign,
+    type: "SELF",
+  });
+
+  const TOKEN_URL = "https://api.commerce.naver.com/external/v1/oauth2/token";
+
+  const response = await fetch(`${TOKEN_URL}?${query}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const { access_token } = await response.json();
+
+  return access_token;
+}
+
+export { generateAccessToken };
