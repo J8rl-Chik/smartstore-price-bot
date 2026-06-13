34d34769cadbd4382e1272f7f428236e2dc00283
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-06-13 18:32:36 +0900

    feat: pueppeteer 로그인 기능 추가
    - 네이버 로그인 페이지로 이동
    - 아이디와 비밀번호 입력 기능 구현
    - 로그인 버튼 클릭 기능 추가

---
+++ login.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+
+export async function login(page) {
+  const ID_SELECTOR = "#id";
+  const PASSWORD_SELECTOR = "#pw";
+  const LOGIN_BUTTON_SELECTOR = ".btn_login";
+  const TYPE_DELAY = 100;
+
+  await page.goto("https://nid.naver.com/");
+
+  await page.click(ID_SELECTOR);
+  await page.type(ID_SELECTOR, process.env.NAVER_ID, { delay: TYPE_DELAY });
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.type(PASSWORD_SELECTOR, process.env.NAVER_PASSWORD, {
+    delay: TYPE_DELAY,
+  });
+
+  await page.click(LOGIN_BUTTON_SELECTOR);
+}
