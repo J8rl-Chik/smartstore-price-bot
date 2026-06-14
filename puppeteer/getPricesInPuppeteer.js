0da03c733bb379caa352399857765e94687354a7
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-06-15 01:10:04 +0900

    feat: getPricesInPuppeteer 함수 개선 및 리팩토링
    - 테스트 코드 추가
    - 불필요한 코드 제거 및 sellerPrices 추출 로직 개선

--- puppeteer/getPricesInPuppeteer.js
+++ puppeteer/getPricesInPuppeteer.js
@@ -1,95 +1,68 @@
-import delaySecond from "../delaySecond.js";
-import naverLogin from "./naverLogin.js";
+import { pathToFileURL } from "node:url";
 
-async function getPricesInPuppeteer(page, url, productName) {
+import createBrowser from "./createBrowser.js";
+import delaySeconds from "../util/delaySeconds.js";
+import { naverLogin } from "./naverLogin.js";
+
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+  createBrowser().then(async ({ page }) => {
+    await naverLogin(page);
+    await delaySeconds(1);
+
+    const productUrl =
+      "https://search.shopping.naver.com/catalog/51929585719?query=%ED%9C%A9%EB%93%9C&NaPm=ct%3Dmqdy6ff4%7Cci%3Da242da32bacc1f085a62dd901ee984ed2ba0e29d%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3Df31dd97911f3b3736f4ad58ff2d1026121c8e95a";
+    const productName = "휩드 머그트리 비건 팩 클렌저 130ml 1개";
+
+    await getPricesInPuppeteer(page, productUrl, productName).then(console.log);
+  });
+}
+
+export default async function getPricesInPuppeteer(page, url, productName) {
   let encodeName = encodeURIComponent(productName).replaceAll("(", "%28");
   encodeName = encodeName.replaceAll(")", "%29");
 
   const referer = `https://search.shopping.naver.com/search/all?query=${encodeName}&vertical=search`;
 
-  await delaySecond(1);
+  await delaySeconds(1);
 
   await page.goto(referer, {
     referer: "https://search.shopping.naver.com/home",
   });
 
-  await delaySecond(1);
+  await delaySeconds(1);
 
   await page.goto(url, {
     referer,
   });
 
-  const result = await page.evaluate(async () => {
-    {
-      const getSeller = (productPriceRow) => {
-        const sellerLink = productPriceRow.querySelector(
-          'span[class^="product_name__"]',
-        );
+  const sellerPrices = await page.evaluate(async () => {
+    const getSeller = (productSellerRow) => {
+      const SELLER_SELECTOR = 'span[class^="product_name__"]';
+      const { textContent: seller } =
+        productSellerRow.querySelector(SELLER_SELECTOR);
 
-        return sellerLink.textContent || sellerLink.querySelector("img").alt;
-      };
+      return seller;
+    };
 
-      const getNaverPayInfo = (productPriceRow) => {
-        const naverPayInfo = productPriceRow.querySelector(".blind");
+    const getPrice = (productSellerRow) => {
+      const PRICE_SELECTOR = 'strong[class^="product_num__"]';
+      const { textContent: price } =
+        productSellerRow.querySelector(PRICE_SELECTOR);
 
-        return naverPayInfo ? naverPayInfo.textContent : null;
-      };
+      return Number(price.replaceAll(",", ""));
+    };
 
-      const getPrice = (productPriceRow) => {
-        const sellingPrice = productPriceRow.querySelector(
-          'strong[class^="product_num__"]',
-        );
+    const productSellerRows = Array.from(
+      document.querySelectorAll('[class^="product_seller_info_wrap__"]'),
+    );
 
-        return Number(
-          [...sellingPrice.textContent]
-            .filter((character) => ![","].includes(character))
-            .join(""),
-        );
-      };
+    const sellerPrices = productSellerRows.map((productSellerRow) => ({
+      seller: getSeller(productSellerRow),
+      price: getPrice(productSellerRow),
+    }));
 
-      const getCouponDiscontPrice = (productPriceRow) => {
-        const sellingPrices = productPriceRow.querySelectorAll("del");
-        if (sellingPrices.length > 1) {
-          return Number(
-            [...sellingPrices[1].textContent]
-              .filter((character) => ![","].includes(character))
-              .join(""),
-          );
-        }
-
-        return Number(
-          [...sellingPrices[0].textContent]
-            .filter((character) => ![","].includes(character))
-            .join(""),
-        );
-      };
-
-      const hasCouponDiscount = (productPriceRow) =>
-        productPriceRow.textContent.includes("쿠폰 할인가");
-
-      const productPriceRows = Array.from(
-        document.querySelectorAll('[class^="product_seller_info_wrap__'),
-      );
-
-      const prices = productPriceRows.map((productPrice) => ({
-        seller: getSeller(productPrice),
-        naverPay: getNaverPayInfo(productPrice),
-        sellingPrice: getPrice(productPrice),
-      }));
-
-      const couponDiscountPrices = productPriceRows
-        .filter(hasCouponDiscount)
-        .map((productPrice) => ({
-          seller: getSeller(productPrice),
-          naverPay: getNaverPayInfo(productPrice),
-          sellingPrice: getCouponDiscontPrice(productPrice),
-        }));
-
-      return [...prices, ...couponDiscountPrices];
-    }
+    return sellerPrices;
   });
 
-  return result;
+  return sellerPrices;
 }
-
-exports.getPricesInPuppeteer = getPricesInPuppeteer;
