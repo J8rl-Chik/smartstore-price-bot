a91829d71ca49bf9cf933d2460668fb0b79bb3cc
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-06-15 13:27:15 +0900

    refactor: getProductRows 함수 리팩토링
    - getProductSheet 함수 제거 후 병합

--- google/getProductRows.js
+++ google/getProductRows.js
@@ -3,32 +3,27 @@ import { pathToFileURL } from "node:url";
 import { google } from "googleapis";
 
 if (import.meta.url === pathToFileURL(process.argv[1]).href) {
-  getProductRows().then(([productRow]) => {
-    console.log(`getProductRows 함수 테스트: ${productRow}`);
+  getProductRows().then((productRows) => {
+    console.log(`getProductRows 함수 테스트: ${productRows.length}개`);
   });
 }
 
 export default async function getProductRows() {
-  const productSheet = await getProductSheet();
-  const CHECK_INDEX = 3;
-  const START_INDEX = 1;
-
-  return productSheet.filter(
-    (productItems) => productItems[CHECK_INDEX] === "TRUE",
-  );
-}
-
-async function getProductSheet() {
   const SHEET_ID = process.env.SHEET_ID;
   const SHEET_NAME = process.env.SHEET_NAME;
+  const COLUMN_COUNT = 12; // A~L
   const sheetsResource = await getSheetsResource();
   const productSheet = await getSheetValues(
     sheetsResource,
     SHEET_ID,
-    `${SHEET_NAME}!A2:M`, // 1번 행은 타이틀 행, 현재 M 칼럼까지만 사용 중
+    `${SHEET_NAME}!A2:L`, // 1번 행은 타이틀 행, 현재 L 칼럼까지만 사용 중
   );
 
-  return productSheet.data.values;
+  const ACTIVATE_INDEX = 1;
+
+  return productSheet.data.values
+    .map((row) => Array.from({ length: COLUMN_COUNT }, (_, i) => row[i] ?? ""))
+    .filter((productItems) => productItems[ACTIVATE_INDEX] === "TRUE");
 }
 
 async function getSheetsResource() {
