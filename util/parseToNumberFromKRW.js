e3dd0d2831ca94c90177fb1fcb608dd4690c4d61
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-07-15 18:41:59 +0900

    refactor: parseToNumberFromKRW의 isZeroAllowed 옵션 제거
    - 0 허용 여부를 옵션으로 분기하던 로직을 단순화해 항상 양의 정수 & 10배수만 검증
    - virtualPrice의 "빈 문자열이면 없음" 처리는 core/productRow.js 쪽 책임으로 이동

--- util/parseToNumberFromKRW.js
+++ util/parseToNumberFromKRW.js
@@ -1,7 +1,6 @@
 import removeComma from './removeComma.js';
 
-// TODO: 0원을 허용 여부 로직 분리를 함수형 FP나 OOP를 참고하여 리팩토링 고려
-export default function parseToNumberFromKRW(krwString, { isZeroAllowed = false } = {}) {
+export default function parseToNumberFromKRW(krwString) {
   const price = Number(removeComma(krwString).replaceAll('₩', ''));
 
   if (Number.isNaN(price)) {
@@ -12,12 +11,7 @@ export default function parseToNumberFromKRW(krwString, { isZeroAllowed = false 
     throw new Error(`"${krwString}"은(는) 정수여야 합니다.`);
   }
 
-  // 가상 판매처 가격은 0원 이상이어야 합니다.
-  if (isZeroAllowed && price < 0) {
-    throw new Error(`"${krwString}"은(는) 0 이상이어야 합니다.`);
-  }
-
-  if (!isZeroAllowed && price < 1) {
+  if (price < 1) {
     throw new Error(`"${krwString}"은(는) 0보다 큰 값이어야 합니다.`);
   }
 
