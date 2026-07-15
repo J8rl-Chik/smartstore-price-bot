c74ae36dac6d4ad56210adf77add29558ee6d5a9
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-07-15 19:23:50 +0900

    refactor: sellers.js를 판매처/가격 처리 책임으로 단순화
    - addVirtualSeller(가상 판매처 객체 생성) 대신 addVirtualPrice로 변경, 가격 숫자만 다루도록 함
      (name 필드는 즉시 버려져서 seller 객체로 감쌀 필요가 없었음)
    - sortByPriceAscending 제거, 정렬 책임은 core/pricing.js의 calculateTargetPrice로 이동
    - filterExcludedSellers 시그니처를 (sellers, myStoreName, excludedSellerNames)에서
      (sellers, excludedSellerNames)로 단순화, 호출부에서 이름 목록을 합쳐서 전달
    - 위 변경에 맞춘 단위 테스트 작성

--- core/sellers.js
+++ core/sellers.js
@@ -1,16 +1,5 @@
-export const filterExcludedSellers = (sellers, myStoreName, excludedSellerNames) => {
-  const allExcludedSellerNames = [myStoreName, ...excludedSellerNames];
+export const filterExcludedSellers = (sellers, excludedSellerNames) =>
+  sellers.filter(({ name }) => !excludedSellerNames.includes(name));
 
-  return sellers.filter(({ name }) => !allExcludedSellerNames.includes(name));
-};
-
-export const addVirtualSeller = (sellers, virtualPrice) => {
-  if (!(virtualPrice > 0)) {
-    return sellers;
-  }
-
-  return [...sellers, { name: '가상 판매처', price: virtualPrice }];
-};
-
-export const sortByPriceAscending = (sellers) =>
-  [...sellers].sort((seller1, seller2) => seller1.price - seller2.price);
+export const addVirtualPrice = (prices, virtualPrice) =>
+  virtualPrice ? [...prices, virtualPrice] : prices;
