c7d483ac68b9defa01d6ff0004f80ac9c562aa03
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-07-15 18:44:52 +0900

    style: parseToNumberFromKRW 파라미터명에서 타입 접미사 제거
    - krwString -> krw

--- util/parseToNumberFromKRW.js
+++ util/parseToNumberFromKRW.js
@@ -1,22 +1,22 @@
 import removeComma from './removeComma.js';
 
-export default function parseToNumberFromKRW(krwString) {
-  const price = Number(removeComma(krwString).replaceAll('₩', ''));
+export default function parseToNumberFromKRW(krw) {
+  const price = Number(removeComma(krw).replaceAll('₩', ''));
 
   if (Number.isNaN(price)) {
-    throw new Error(`"${krwString}"을(를) 숫자로 변환할 수 없습니다.`);
+    throw new Error(`"${krw}"을(를) 숫자로 변환할 수 없습니다.`);
   }
 
   if (!Number.isInteger(price)) {
-    throw new Error(`"${krwString}"은(는) 정수여야 합니다.`);
+    throw new Error(`"${krw}"은(는) 정수여야 합니다.`);
   }
 
   if (price < 1) {
-    throw new Error(`"${krwString}"은(는) 0보다 큰 값이어야 합니다.`);
+    throw new Error(`"${krw}"은(는) 0보다 큰 값이어야 합니다.`);
   }
 
   if (price % 10 !== 0) {
-    throw new Error(`"${krwString}"은(는) 10의 배수여야 합니다.`);
+    throw new Error(`"${krw}"은(는) 10의 배수여야 합니다.`);
   }
 
   return price;
