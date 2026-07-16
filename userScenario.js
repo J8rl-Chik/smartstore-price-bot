a74c240cae9b940a94877b003700a4ae0234f87c
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-07-16 17:28:36 +0900

    refactor: getSaleProducts 호출 방식 변경
    - saleProducts에서 saleProduct를 찾는 로직 추가

--- userScenario.js
+++ userScenario.js
@@ -14,7 +14,7 @@ import { calculateTargetPrice, isUpdateRequired } from './core/pricing.js';
 import { buildPriceWithDeliveryFee } from './core/delivery.js';
 
 const runUserScenario = async () => {
-  const [saleProduct] = await getSaleProducts();
+  const saleProducts = await getSaleProducts();
   const productRows = await getProductRows();
   const page = await createPage();
   const myStoreName = process.env.SMART_STORE_NAME;
@@ -22,16 +22,17 @@ const runUserScenario = async () => {
   await naverLogin(page);
   await delaySeconds(1);
 
+  const saleProduct = saleProducts.find((saleProduct) => {
+    const productName = getProductName(saleProduct);
+    const matchedRow = findProductRow(productRows, productName);
+
+    return matchedRow ? true : false;
+  });
+
   const productName = getProductName(saleProduct);
   const matchedRow = findProductRow(productRows, productName);
 
   console.log(productName);
-
-  if (!matchedRow) {
-    console.log('일치하는 제품 행이 없습니다.');
-
-    return;
-  }
 
   const priceRule = parseProductRow(matchedRow);
   const sellers = await getSellersInPuppeteer(page, priceRule.catalogUrl, productName);
