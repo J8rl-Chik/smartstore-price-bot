3da744028911ac2c1bfc6c0c99285c3aa5e9bb9b
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-09-06 18:45:23 +0900

    refactor: 도메인별로 관리할 수 있도록 폴더로 이동

--- src/domain/filterExcludedSellers.ts
+++
@@ -1,9 +0,0 @@
-export interface Seller {
-  name: string;
-  price: number;
-  deliveryFee: number;
-  deliveryFeeType: string;
-}
-
-export const filterExcludedSellers = (sellers: Seller[], excludedSellerNames: string[]): Seller[] =>
-  sellers.filter(({ name }) => !excludedSellerNames.includes(name));
